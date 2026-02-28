d4ada68ee537401ad0825610e5e81dfc55d2dfd8
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 18:32:51 -0500

    backend.tf update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-infra"
-    storage_account_name = "infrastorageacct"
+    storage_account_name = "infrastorageacctpjdelta"
     container_name       = "infra-tfstate"
     key                  = "infra.terraform.tfstate"
   }
