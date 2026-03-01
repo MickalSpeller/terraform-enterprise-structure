720ce221a364342c44c1c8af14a7df65e17ecb9c
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 20:16:37 -0500

    commite

--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ resource "azurerm_virtual_network" "vnet-hub01" {
   location            = azurerm_resource_group.rg-infra.location
   resource_group_name = azurerm_resource_group.rg-infra.name
 }
+resource "azurerm_subnet" "subnet-hub01" {
+  name                 = "subnet-hub01"
+  resource_group_name  = azurerm_resource_group.rg-infra.name
+  virtual_network_name = azurerm_virtual_network.vnet-hub01.name
+  address_prefixes     = ["10.0.1.0/24"] 
+}
+
