e278ebfc6574dcd3ca75c6c51dd787385498bfb4
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 19:52:16 -0500

    commit

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "azurerm_resource_group" "rg-infra" { 
+resource "azurerm_resource_group" "resource_group_infrastructure_state" { 
   name     = "rg-project-delta"
   location = "eastus"
+}
+moved {
+  from = azurerm_resource_group.resource_group_infrastructure_state
+  to   = azurerm_resource_group.rg-infra
 }
