30e69056999da80d6d1ad45efc2a818c7030fe73
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 23:05:27 -0500

    commit

--- main.tf
+++ main.tf
@@ -12,3 +12,27 @@ resource "azurerm_virtual_network" "vnet-hub01" {
   location            = azurerm_resource_group.rg-infra.location
   resource_group_name = azurerm_resource_group.rg-infra.name
 }
+
+resource "azurerm_linux_virtual_machine" "vm01" {
+  name                = "vm01"
+  resource_group_name = azurerm_resource_group.rg-infra.name
+  location            = azurerm_resource_group.rg-infra.location
+  size                = "Standard_F2"
+  admin_username      = "labadministrator"
+  admin_password = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
