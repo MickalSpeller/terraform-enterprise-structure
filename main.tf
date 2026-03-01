b272d3901de2ae59e892364e1e235ad89c296263
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 19:38:24 -0500

    commit

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-resource "azurerm_resource_group" "rg-infra-state" { 
+resource "azurerm_resource_group" "resource_group_infrastructure_state" { 
   name     = "rg-project-delta"
   location = "eastus"
-}
-resource "azurerm_virtual_network" "vnet-hub" {
-  name                = "vnet-hub"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg-infra-state.location
-  resource_group_name = azurerm_resource_group.rg-infra-state.name
 }
