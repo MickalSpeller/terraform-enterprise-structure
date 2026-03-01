21da7f8e19795594267ff4c4d57d7e22acfb6b69
MickalSpeller <mickal.speller@icloud.com>
2026-03-01 10:50:02 -0500

    commit

--- modules/vwan/main.tf
+++ modules/vwan/main.tf
@@ -1,32 +1,32 @@
 resource "azurerm_virtual_wan" "vwan" {
   name                = "vwan-global"
   location            = var.east_region
-  resource_group_name = azurerm_resource_group.rg-infra.name
+  resource_group_name = "rg-project-delta"
 }
 resource "azurerm_virtual_hub" "east_hub" {
   name                = "vhub-east"
   location            = var.east_region
-  resource_group_name = azurerm_resource_group.rg-infra.name
+  resource_group_name = "rg-project-delta"
   virtual_wan_id      = azurerm_virtual_wan.vwan.id
   address_prefix      = "10.0.0.0/23"
 }
 resource "azurerm_virtual_hub" "west_hub" {
   name                = "vhub-west"
   location            = var.west_region
-  resource_group_name = azurerm_resource_group.rg-infra.name
+  resource_group_name = "rg-project-delta"
   virtual_wan_id      = azurerm_virtual_wan.vwan.id
   address_prefix      = "10.1.0.0/23"
 }
 resource "azurerm_virtual_network" "east_vnet" {
   name                = "vnet-east"
   location            = var.east_region
-  resource_group_name = azurerm_resource_group.rg-infra.name
+  resource_group_name = "rg-project-delta"
   address_space       = ["10.10.0.0/16"]
 }
 resource "azurerm_virtual_network" "west_vnet" {
   name                = "vnet-west"
   location            = var.west_region
-  resource_group_name = azurerm_resource_group.rg-infra.name
+  resource_group_name = "rg-project-delta"
   address_space       = ["10.20.0.0/16"]
 }
 resource "azurerm_virtual_hub_connection" "east_connection" {
