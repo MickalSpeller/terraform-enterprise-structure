4aaab8893f75b5bf7ffa1d1cae1f5e2e953597a5
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 20:10:15 -0500

    commit

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ moved {
   from = azurerm_resource_group.resource_group_infrastructure_state
   to   = azurerm_resource_group.rg-infra
 }
+resource "azurerm_virtual_network" "vnet-hub01" {
+  name                = "vnet-hub01"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-infra.location
+  resource_group_name = azurerm_resource_group.rg-infra.name
+}
