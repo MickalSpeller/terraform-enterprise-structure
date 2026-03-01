e11411e150e741efb7041191207d6cfbd77bb3ef
MickalSpeller <mickal.speller@icloud.com>
2026-02-28 21:12:17 -0500

    commit

--- main.tf
+++ main.tf
@@ -30,3 +30,9 @@ resource "azurerm_subnet" "subnet-spoke01" {
   virtual_network_name = azurerm_virtual_network.vnet-spoke01.name
   address_prefixes     = ["10.1.1.0/24"]
 }
+resource "azurerm_subnet" "snet-02" {
+ name                  = "snet-02"
+ resource_group_name   = azurerm_resource_group.rg-infra.name
+ virtual_network_name  = azurerm_virtual_network.vnet-spoke01.name
+ address_prefixes      = ["10.1.2.0/24"]
+}
